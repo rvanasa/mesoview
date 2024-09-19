c45b55555e910a4588cc28f9f656153bfe5195ae
Ryan Vandersmith <ryanvandersmith@gmail.com>
2024-09-19 13:45:54 -0600

    Misc

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-const rootElement = document.getElementById('root')!;
-const root = createRoot(rootElement);
-
+const root = createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />

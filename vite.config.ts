b0ceae71ae8b88584cf8242200490fa4d19527c4
Ryan Vandersmith <ryanvandersmith@gmail.com>
2024-09-19 18:55:35 -0600

    Remove PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
         plugins: ['babel-plugin-twin', 'babel-plugin-macros'],
       },
     }),
-    VitePWA({ registerType: 'autoUpdate' }),
   ],
   css: {
     preprocessorOptions: {

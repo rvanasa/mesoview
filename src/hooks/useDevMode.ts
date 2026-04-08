9feb0266068a0bc8ed83c5c26184cdf1c5197158
Ryan Vandersmith <ryanvandersmith@gmail.com>
2026-04-08 09:14:46 -0400

    Add query param to enable dev mode

--- src/hooks/useDevMode.ts
+++ src/hooks/useDevMode.ts
@@ -1,7 +1,26 @@
 import { useLocalStorage } from 'usehooks-ts';
 
+const localStorageKey = 'mesoview.devMode';
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has('dev')) {
+  const devValue = urlParams.get('dev');
+  if (devValue === 'false') {
+    localStorage.removeItem(localStorageKey);
+  } else {
+    localStorage.setItem(localStorageKey, 'true');
+  }
+
+  urlParams.delete('dev');
+  const newSearch = urlParams.toString();
+  const newUrl =
+    window.location.pathname +
+    (newSearch ? '?' + newSearch : '') +
+    window.location.hash;
+  window.history.replaceState({}, '', newUrl);
+}
+
 export default function useDevMode(): [boolean, (value: boolean) => void] {
-  const [isDevMode, setDevMode] = useLocalStorage('mesoview.devMode', false);
+  const [isDevMode, setDevMode] = useLocalStorage(localStorageKey, false);
 
   return [isDevMode, setDevMode];
 }
